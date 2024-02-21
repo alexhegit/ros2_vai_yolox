ffcbf458c04f67f61ff432d6d5de6fd526b897e8
Alex He <alehe@amd.com>
2024-02-22 02:32:44 +0800

    First release version
    Signed-off-by: Alex He  <heye_dev@163.com>

---
+++ README.md
@@ -0,0 +1,54 @@
+# ros2_vai_yolox
+------------------
+A ROS2 node using YOLOX do inference with Vitis AI and ONNX runtime. You can deploy it with AMD Ryzen AI CPU to leveage the IPU to accelerate the inference.
+This node subscribes the image msg from usb_cam and publish the inference image. And you can also modify it for other hardware EP of ONNX runtime.
+
+## Prerequist
+
+### Platform
+1. Ryzen AI CPU Platform (e.g. Miniform Venus UM790 Pro/UM760 Pro with Ryzen 7940HS)
+2. Ubuntu22.04
+3. usb camera (e.g. logi-c270)
+
+### Software
+1. Setup Ryzen AI(VitisAI) software environment (refer to VitisAI documentattion to install the stack)
+2. Install ROS2-humble (refer to https://docs.ros.org/en/humble/index.html)
+3. Install ros2 usb_cam package (refer to https://index.ros.org/p/usb_cam/)
+> sudo apt install ros-humble-usb-cam
+
+## Build the ros2_vai_yolox package
+1. source the ros2-humble environment
+> source /opt/ros/humble/setup.sh
+2. Clone the repo
+> git clone https://gitenterprise.xilinx.com/alehe/ros2_vai_yolox.git
+
+3. Build package
+> The package include one node name /vai_yolox
+> cd ros2_vai_yolox/ros2_vai_ws/
+> colcon build
+
+3. Check the ros2_vai_yolox package
+> source install/local_setup.sh
+> $ ros2 pkg list | grep vai_yolox
+>> vai_yolox_pkg
+
+## Run the demo
+The demo pipeline is below. the /usb_cam node publish iamge_raw topic and /vai_yolox node subscribe the image_raw and publish the inference result as /image_infer topic.
+
+**Steps**
+The vai yolox inference depends on the IPU config file (in ros2_vai_ws/src/vai_yolox_pkg/resource/vaip_config.json) and yolox-s-int8.onnx. You should copy them to /tmp folder.(Now there are some hard codes to find them). The yolox-s-int8.onnx could be downloaded from https://huggingface.co/amd/yolox-s.
+
+1. Open terminal 1 and run the /usb_cam
+> ros2 run usb_cam usb_cam_node_exe
+
+2. Open terminal 2 and run /vai_yolox
+> ros2 run vai_yolox_pkg yolox_node
+
+3. Open terminal 3 to check the pipeline graph
+> ros2 run rqt_graph rqt_graph
+
+4. Open terminal 4 to view the runtime inference video
+> ros2 run rqt_image_view rqt_image_view
+
+The whole demo looks like
+
---
+++ ros2_vai_ws/src/vai_yolox_pkg/vai_yolox_pkg/yolox_node.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import cv2
+import pathlib
+import numpy as np
+import onnxruntime as ort
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+
+CURRENT_DIR = pathlib.Path(__file__).parent
+sys.path.append(str(CURRENT_DIR))
+
+from coco import COCO_CLASSES
+from demo_utils import mkdir, multiclass_nms, demo_postprocess, vis
+
+
+class VAI_YOLOX(Node):
+    def __init__(self, name):
+        super().__init__(name)
+        self.get_logger().info("AMD VAI YOLOX inference node init")
+        self.score_thr = 0.3
+        self.output_dir = "demo_output"
+        self.image_path = "demo.jpg"
+        self.model = "yolox-s-int8.onnx"
+        self.ipu = "True"
+        self.vaip_config = "/tmp/vaip_config.json"
+        self.bridge = CvBridge()
+        self.sub_img = self.create_subscription(
+            Image,
+            '/image_raw',
+            self.img_infer,
+            10)
+        self.pub_img = self.create_publisher(Image, 'image_infer', 10)
+
+    def img_infer(self, msg):
+        # Convert ROS Image message to OpenCV image
+        origin_img = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+        #origin_img = cv2.imread(self.image_path)
+
+        # Preprocess the image here
+        input_shape = (640, 640)  # replace with your actual input shape
+        img, ratio = self.preprocess(origin_img, input_shape)
+
+        # You can now use padded_img in your inference code...
+        if self.ipu == "True":
+            #self.get_logger().info("IPU EP")
+            providers = ["VitisAIExecutionProvider"]
+            provider_options = [{"config_file": self.vaip_config}]
+        else:
+            self.get_logger().info("CPU EP")
+            providers = ['CPUExecutionProvider']
+            provider_options = None
+        #model = os.path.join(str(CURRENT_DIR), self.model)
+        model = "/tmp/yolox-s-int8.onnx"
+        session = ort.InferenceSession(model, providers=providers, provider_options=provider_options)
+        # ort_inputs = {session.get_inputs()[0].name: img[None, :, :, :]}
+        ort_inputs = {session.get_inputs()[0].name: np.transpose(img[None, :, :, :], (0, 2 ,3, 1))}
+        outputs = session.run(None, ort_inputs)
+        outputs = [np.transpose(out, (0, 3, 1, 2)) for out in outputs]
+        dets = self.postprocess(outputs, input_shape, ratio)
+        if dets is not None:
+            final_boxes, final_scores, final_cls_inds = dets[:, :4], dets[:, 4], dets[:, 5]
+            origin_img = vis(origin_img, final_boxes, final_scores, final_cls_inds,
+                         conf=self.score_thr, class_names=COCO_CLASSES)
+            frame = cv2.resize(origin_img, (640, 480))
+		    # opencv mat ->  ros msg
+            msg = self.bridge.cv2_to_imgmsg(frame, "bgr8")
+            self.pub_img.publish(msg)
+
+        mkdir(self.output_dir)
+        output_path = os.path.join(self.output_dir, os.path.basename(self.image_path))
+
+        cv2.imwrite(output_path, origin_img)
+
+    def preprocess(self, img, input_shape, swap=(2, 0, 1)):
+        """
+        Preprocessing part of YOLOX for scaling and padding image as input to the network.
+
+        Args:
+        img (numpy.ndarray): H x W x C, image read with OpenCV
+        input_shape (tuple(int)): input shape of the network for inference
+        swap (tuple(int)): new order of axes to transpose the input image
+
+        Returns:
+        padded_img (numpy.ndarray): preprocessed image to be fed to the network
+        ratio (float): ratio for scaling the image to the input shape
+        """
+        if len(img.shape) == 3:
+            padded_img = np.ones((input_shape[0], input_shape[1], 3), dtype=np.uint8) * 114
+        else:
+            padded_img = np.ones(input_shape, dtype=np.uint8) * 114
+        ratio = min(input_shape[0] / img.shape[0], input_shape[1] / img.shape[1])
+        resized_img = cv2.resize(
+            img,
+            (int(img.shape[1] * ratio), int(img.shape[0] * ratio)),
+            interpolation=cv2.INTER_LINEAR,
+        ).astype(np.uint8)
+        padded_img[: int(img.shape[0] * ratio), : int(img.shape[1] * ratio)] = resized_img
+        padded_img = padded_img.transpose(swap)
+        padded_img = np.ascontiguousarray(padded_img, dtype=np.float32)
+        return padded_img, ratio
+
+    def postprocess(self, outputs, input_shape, ratio):
+        """
+        Post-processing part of YOLOX for generating final results from outputs of the network.
+
+        Args:
+            outputs (tuple(numpy.ndarray)): outputs of the detection heads with onnxruntime session
+            input_shape (tuple(int)): input shape of the network for inference
+            ratio (float): ratio for scaling the image to the input shape
+
+        Returns:
+            dets (numpy.ndarray): n x 6, dets[:,:4] -> boxes, dets[:,4] -> scores, dets[:,5] -> class indices
+        """
+        outputs = [out.reshape(*out.shape[:2], -1).transpose(0,2,1) for out in outputs]
+        outputs = np.concatenate(outputs, axis=1)
+        outputs[..., 4:] = sigmoid(outputs[..., 4:])
+        predictions = demo_postprocess(outputs, input_shape, p6=False)[0]
+        boxes = predictions[:, :4]
+        scores = predictions[:, 4:5] * predictions[:, 5:]
+        boxes_xyxy = np.ones_like(boxes)
+        boxes_xyxy[:, 0] = boxes[:, 0] - boxes[:, 2]/2.
+        boxes_xyxy[:, 1] = boxes[:, 1] - boxes[:, 3]/2.
+        boxes_xyxy[:, 2] = boxes[:, 0] + boxes[:, 2]/2.
+        boxes_xyxy[:, 3] = boxes[:, 1] + boxes[:, 3]/2.
+        boxes_xyxy /= ratio
+        dets = multiclass_nms(boxes_xyxy, scores, nms_thr=0.45, score_thr=0.1)
+        return dets
+
+def sigmoid(x):
+    return 1.0 / (1.0 + np.exp(-x))
+
+
+def main(args=None):
+    print(str(CURRENT_DIR))
+    rclpy.init(args=args)
+    vai_detector = VAI_YOLOX("vai_yolox")
+    rclpy.spin(vai_detector)
+    vai_detector.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+
